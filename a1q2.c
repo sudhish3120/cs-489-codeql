ca98efa584535ba8d0aa0812f36aaaa6c9496b88
Sudhish Meenakshisundaram <sudhish.3120@gmail.com>
2025-02-21 19:39:33 -0500

    security issues fixed

--- a1q2.c
+++ a1q2.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-    
+#include <sys/stat.h>
+#include <errno.h>
+
 #define BUFSIZE 256
-    
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -10,7 +12,13 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    struct stat st;
+    if (stat(argv[1], &st) != 0) {
+        fprintf(stderr, "Error accessing the file\n");
+        return -1;
+    }
+
+    printf("%d\n", st.st_size);
+    return 0;
 }

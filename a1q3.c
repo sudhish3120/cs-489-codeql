55c592df03a6452bee8e64d45cd9eaeb6bcc30fb
Sudhish Meenakshisundaram <sudhish.3120@gmail.com>
2025-02-21 19:58:47 -0500

    q3 of a1-fixed compilation issues

--- a1q3.c
+++ a1q3.c
@@ -16,6 +16,16 @@ int read_file(const char *filename) {
     while (fgets(buff, BUFFSIZE, ptr) != NULL) {
         printf("%s", buff);
     }
-        
+    
+    fclose(ptr);
     return 0; 
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    
+    return read_file(argv[1]);
 }
